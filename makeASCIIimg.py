f3a07fd2bc47367580d9639d794b448a796f404f
Raman-Shakya <raman.gwarko15@gmail.com>
2023-02-08 18:06:11 +0545

    character image map

--- makeASCIIimg.py
+++ makeASCIIimg.py
@@ -1,16 +1,18 @@
-import cv2
-import numpy
+from PIL import Image, ImageDraw, ImageFont
 
-WIDTH = 12
-HEIGHT = WIDTH*2
+WIDTH = 20
+HEIGHT = 40
 
-img = numpy.zeros((2*WIDTH, 242*WIDTH, 1), numpy.uint8)
+image = Image.new('RGB', (257*WIDTH, HEIGHT))
+
+d1 = ImageDraw.Draw(image)
+
+path = "UbuntuMono-R.ttf"
+
+myFont = ImageFont.truetype(path, HEIGHT)
 
 cnt = 0
-for i in range(2,256):
-    if not chr(i).isspace():
-        cv2.putText(img, chr(i), (cnt*WIDTH, HEIGHT), cv2.FONT_HERSHEY_SIMPLEX, 0.8, 255)
-        cnt+=1
+for i in list(range(33,127)) + list(range(160, 257)):
+    d1.text((i*WIDTH,0), chr(i), fill =(255, 255, 255),font=myFont)
 
-cv2.imshow('win', img)
-cv2.waitKey(0)
+image.save("characters.png")
